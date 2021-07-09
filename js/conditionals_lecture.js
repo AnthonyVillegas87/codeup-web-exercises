df8fff078785bdbda0ba1c2809510275f81ce3af
Anthony Villegas <villegasanthony87@yahoo.com>
2021-07-09 10:45:32 -0500

    ternary operator

--- js/conditionals_lecture.js
+++ js/conditionals_lecture.js
@@ -1,6 +1,6 @@
 
 "use strict"
- let flavor = prompt("Welcome to Codeup Ice Cream! What flavor do you want?");
+ //let flavor = prompt("Welcome to Codeup Ice Cream! What flavor do you want?");
 
 // if - else if - else
 
@@ -30,12 +30,27 @@
 //         alert("We do not have that flavor, sorry!");
 // }
 
-function getIceCreamOrder(flavor) {
-    if(flavor === "chocolate" || flavor === "vanilla" || flavor === "strawberry") {
-        alert(`One ${flavor} coming right up!`);
-    } else {
-        alert("We do not have that flavor. Sorry! ");
-    }
+// function getIceCreamOrder() {
+//     return prompt("Welcome to Codeup Ice Cream! What flavor do you want?");
+// }
+//
+// function replyToCustomer(flavor) {
+//     if(flavor === "chocolate" || flavor === "vanilla" || flavor === "strawberry") {
+//         alert(`One ${flavor} coming right up!`);
+//     } else {
+//         alert("We do not have that flavor. Sorry! ");
+//     }
+//
+// }
+// replyToCustomer(getIceCreamOrder())
 
-}
-getIceCreamOrder(flavor)
+let decision = confirm("Are you sure you want to close this page?!")
+//alert(decision)
+
+// if(decision) {
+//     alert("Okay, closing the page...");
+// } else {
+//     alert("Yay! Leaving page open...");
+//}
+
+alert((decision === true)? "Okay, closing the page..." : "Yay! Leaving page open..." );
