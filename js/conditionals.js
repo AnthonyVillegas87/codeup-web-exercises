a00e077d883100ae40a6364fd39be9192a1b4c31
Anthony Villegas <villegasanthony87@yahoo.com>
2021-07-12 11:21:39 -0500

    conditional bonuses

--- js/conditionals.js
+++ js/conditionals.js
@@ -195,33 +195,113 @@
 
 
 
- const displayMessage = (message = confirm("Would you like to enter a number?")) => {
-     let answer = parseFloat(prompt("Please enter a number"))
-   if(!answer) {
-       alert("Not a number")
-       displayMessage()
-   }
-    if(answer % 2 === 0) {
-        alert("Your number is even")
-    } else if (answer % 2 === 1) {
-            alert("Your number is odd")
-    }
-    function myNumber(answer) {
-        return alert(`Your number plus 100 is  ${answer + 100}` )
-    }
-    myNumber(answer)
+ // const displayMessage = (message = confirm("Would you like to enter a number?")) => {
+ //     let answer = parseFloat(prompt("Please enter a number"))
+ //   if(!answer) {
+ //       alert("Not a number")
+ //       displayMessage()
+ //   }
+ //    if(answer % 2 === 0) {
+ //        alert("Your number is even")
+ //    } else if (answer % 2 === 1) {
+ //            alert("Your number is odd")
+ //    }
+ //    function myNumber(answer) {
+ //        return alert(`Your number plus 100 is  ${answer + 100}` )
+ //    }
+ //    myNumber(answer)
+ //
+ //    function negativeOrPositive(answer) {
+ //        if(answer < 0) {
+ //            return alert(`The number ${answer} you entered is negative`)
+ //        } else if(answer > 0) {
+ //            return alert(`The number ${answer} you entered is positive`)
+ //        }
+ //    }
+ //    negativeOrPositive(answer)
+ // }
+ // displayMessage()
 
-    function negativeOrPositive(answer) {
-        if(answer < 0) {
-            return alert(`The number ${answer} you entered is negative`)
-        } else if(answer > 0) {
-            return alert(`The number ${answer} you entered is positive`)
-        }
-    }
-    negativeOrPositive(answer)
- }
- displayMessage()
 
+// ================================= CONDITIONALS BONUSES
+
+// Bonus 1
+// Create a function that prompts a user for their favorite day of the week and alerts a unique message based on the day.
+// - Catch any invalid inputs.
+// - Write the logic using if/else ifs and then refactor using a switch case
+
+function favoriteDay(day = prompt("What is you favorite day of the week?!")) {
+    //let weekDay = day === "Monday" || day === "Tuesday" || day === "Wednesday" || day === "Thursday" || day === "Friday" || day === "Saturday" || day === "Sunday"
+    if(day === "Monday") {
+        alert(`${day} is the beginning of the week, let's go!!`)
+    } else if(day === "Tuesday") {
+        alert(`${day}, we've got the ball rolling!`)
+    } else if(day === "Wednesday") {
+        alert(`${day} means we're halfway there!!`)
+    } else if(day === "Thursday") {
+        alert(`${day}, take a sigh of relief.`)
+    } else if(day === "Friday") {
+        alert(`TGI${day}!`)
+    } else if(day === "Saturday") {
+        alert(`Put your feet up, its ${day}!`)
+    } else if(day === "Sunday") {
+        alert(`${day}! Let's get ready for the week.`)
+    }
+}
+
+favoriteDay()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
+//     Bonus 2
+// Create a function that prompts the user for an input and then alerts if the input is a number or not a number.
+// - use an if/else
+//     - refactor to use a ternary operator
+//
+// Bonus 3
+// Create a function that prompts a user for a season (Spring, Summer, Fall (or Autumn), Winter). The function then alerts the months available in that season and then asks the user to pick a given month. After selecting the month, alert a unique message for the month.
+// - account for any invalid user input
+// - case of input should not matter
+// - accept both abbreviations and full names of the months
+//
+//
+// GOLD STAR BONUS
+//
+// Create a distance unit conversion application.
+//     Prompt the user for unit of measurement.
+//     Prompt the user for a value.
+//
+//     Prompt the user for a second unit of measurement to convert to.
+//
+//     Possibly define multiple functions to convert: inchesToFeet, feetToMiles, milesToLightYears
+// then the opposite versions: lightYearsToMiles, milesToFeet, feetToInches
+//
+// Use these conversion functions to make the correct unit conversion
+//
+// Potentially, you will need a large switch case to account for the possible unit conversions,
+//     i.e. what conversion functions will need to be called in and in what order.
+//
+//     DOUBLE GOLD STAR BONUS
+//
+//     Allow unit conversion to metric system units
+//
 
 
 
