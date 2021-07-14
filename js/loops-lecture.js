3ef2f13cdf76528e2aba4eb2bc6f3984005101f2
Anthony Villegas <villegasanthony87@yahoo.com>
2021-07-14 09:28:57 -0500

    while loops

--- js/loops-lecture.js
+++ js/loops-lecture.js
@@ -1,1 +1,10 @@
 "use strict"
+ var pizzaSlices = 4;
+
+while(pizzaSlices > 0) {
+    console.log("I'll have a slice!");
+    pizzaSlices = pizzaSlices -1;
+    console.log("Now there's " + pizzaSlices + " slices left");
+}
+
+console.log("There's no more pizza left!");
