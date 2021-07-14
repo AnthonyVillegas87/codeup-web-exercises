9b03856731cca5a8011f683820046c4f76ef4dae
Anthony Villegas <villegasanthony87@yahoo.com>
2021-07-14 10:17:31 -0500

    while loop conclusion

--- js/loops-lecture.js
+++ js/loops-lecture.js
@@ -1,10 +1,46 @@
 "use strict"
- var pizzaSlices = 4;
 
-while(pizzaSlices > 0) {
-    console.log("I'll have a slice!");
-    pizzaSlices = pizzaSlices -1;
-    console.log("Now there's " + pizzaSlices + " slices left");
+
+// WHILE LOOP is JavaScripts most basic loop
+
+//  var pizzaSlices = 4;
+//
+// while(pizzaSlices > 0) {
+//     console.log("I'll have a slice!");
+//     pizzaSlices = pizzaSlices -1;
+//     console.log("Now there's " + pizzaSlices + " slices left");
+// }
+//
+// console.log("There's no more pizza left!");
+
+// Count Up
+// var counter = 0;
+// while(counter <= 100) {
+//     console.log(counter);
+//     counter++;
+// }
+
+// Count Down
+// var counter = 100
+// while(counter >= 0) {
+//     console.log(counter);
+//     counter--;
+// }
+
+// var counter = 1;
+// var number = 5;
+// while (counter <= 5) {
+//     number = number * 2;
+//     console.log(number);
+//     counter++;
+// }
+
+let count = 0; //variable starts at 0
+while(count < 10) {  // once the expression has executed 10 times the expression becomes false
+    console.log(count);
+    count++;
 }
 
-console.log("There's no more pizza left!");
+/// NOTE THAT YOU CAN CREATE AN INFINITE LOOP WITH THE SYNTAX :: while(true) ///
+
+
