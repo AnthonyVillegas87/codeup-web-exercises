95dc7b848ad4ba4ea0d73fa2af1e04064dead3c3
Anthony Villegas <villegasanthony87@yahoo.com>
2021-07-15 11:35:43 -0500

    for looops exercise

--- js/while.js
+++ js/while.js
@@ -8,28 +8,28 @@
 //     }
 
 
-    var allCones = Math.floor(Math.random() * 50) + 50;
-    confirm(`You have ${allCones} ice cream cones to sell today!`);
-
-    function sellCones() {
-        let conesSold = Math.floor(Math.random() * 5) + 1;
-        while(conesSold - allCones) {
-            confirm(`You sold ${conesSold} so far!`);
-            do {
-                allCones = allCones - conesSold
-
-                if (conesSold < allCones) {
-                    confirm(`You have ${allCones} left to sell`);
-
-                } else {
-                    alert("Yay i sold them all!");
-                    break;
-                }
-            } while(allCones > conesSold);
-
-        }
-    }
- sellCones()
+ //    var allCones = Math.floor(Math.random() * 50) + 50;
+ //    confirm(`You have ${allCones} ice cream cones to sell today!`);
+ //
+ //    function sellCones() {
+ //        let conesSold = Math.floor(Math.random() * 5) + 1;
+ //        while(conesSold - allCones) {
+ //            confirm(`You sold ${conesSold} so far!`);
+ //            do {
+ //                allCones = allCones - conesSold
+ //
+ //                if (conesSold < allCones) {
+ //                    confirm(`You have ${allCones} left to sell`);
+ //
+ //                } else {
+ //                    alert("Yay i sold them all!");
+ //                    break;
+ //                }
+ //            } while(allCones > conesSold);
+ //
+ //        }
+ //    }
+ // sellCones()
 
 
     //// SOLUTION TO CONES EXERCISE
@@ -41,13 +41,6 @@
     //     console.log(allCones + " cones left");
     //     allCones = allCones - conesSold;
     // } while(allCones >= 0)
-
-
-
-
-
-
-
 
 
 
