bbdc5ddd41cddce98d43643bbe349ad208f1b639
Anthony Villegas <villegasanthony87@yahoo.com>
2021-07-17 13:53:26 -0500

    nested for loops in progress

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -21,14 +21,27 @@
     //
     // }
 
-    for(var i = 0; i <= 50; i++) {
-         i = prompt("Pick a number between 1 & 50!");
-        if(i % 2 === 0) {
-            continue;
-        }
-        console.log("Here is an odd number: " + i);
-    }
+    // for(var i = 0; i <= 50; i++) {
+    //      i = prompt("Pick a number between 1 & 50!");
+    //     if(i % 2 === 1) {
+    //         console.log("Here is an odd number: " + i);
+    //         continue;
+    //     }
+    //
+    // }
 
+    ////// IN PROGRESS ////
+    // for(var i = 0; i <= 50; i -= 1) {
+    //      i = prompt("Pick a number between 1 & 50!");
+    //      for(var j = 0; j <= i; j++) {
+    //          console.log("Here is an odd number: " + i)
+    //          if (j === i) {
+    //              console.log("Yikes, skip this number: " + j);
+    //              break;
+    //          }
+    //      }
+    //    //continue;
+    // }
 
 
 
