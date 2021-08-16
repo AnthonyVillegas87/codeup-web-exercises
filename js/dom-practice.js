52ae7864cb619871ae5b322417def1b3406ba9dc
Anthony Villegas <villegasanthony87@yahoo.com>
2021-08-15 19:48:19 -0500

    feat: DOM practice

---
+++ js/dom-practice.js
@@ -0,0 +1,18 @@
+"use strict";
+let boxes = document.querySelectorAll('.display');
+
+function changeColor() {
+    // let letters = '0123456789';
+    let randomColor = 'rgb('
+    for(let i = 0; i < 3; i++) {
+      randomColor += [Math.floor(Math.random() * 256)] + ',';
+    }
+    return randomColor.substring(0, randomColor.length -1) + ')'
+}
+
+console.log(changeColor())
+boxes.forEach(function (box) {
+    box.addEventListener('mouseover', function() {
+        box.style.backgroundColor = changeColor()
+    })
+})
