b01bfdbea5d857712dda2cbb47f01a0c02c63204
Anthony Villegas <villegasanthony87@yahoo.com>
2021-08-15 23:41:38 -0500

    feat: traverse dom practice

--- js/traverse_dom.js
+++ js/traverse_dom.js
@@ -1,1 +1,13 @@
 "use strict"
+let article = document.getElementById('articles');
+let mainDiv = document.getElementById('main');
+
+
+article.style['font-size'] = '50px';
+article.children[0].lastElementChild.style.color = 'blue';
+article.previousElementSibling.style.backgroundColor = 'black';
+
+mainDiv.children[1].style.textDecoration = "underline";
+mainDiv.lastElementChild.style['font-size'] = '50px';
+mainDiv.children[0].style.color = 'orange';
+mainDiv.lastElementChild.parentElement.style['font-size'] = '40px';
