9adf32b05f075d7afa1967417bf06bf16a6ad88f
Anthony Villegas <villegasanthony87@yahoo.com>
2021-08-20 10:27:23 -0500

    feat: api data logged for weather map

--- js/weather.js
+++ js/weather.js
@@ -1,6 +1,18 @@
 $(document).ready(() => {
     "use strict";
 
+    $.get('https://api.openweathermap.org/data/2.5/onecall', {
+        appid: WEATHER_API_TOKEN,
+        lat: 29.4252,
+        lon: -98.4916,
+        exclude: 'minutely,hourly,alerts',
+        units: 'imperial',
+        language: 'en'
+    }).done((data) => {
+        console.log(data);
+    }).fail((error) => {
+        console.log(error);
+    });
 
     mapboxgl.accessToken = MAPBOX_API_TOKEN;
     const map = new mapboxgl.Map({
@@ -9,12 +21,6 @@ $(document).ready(() => {
         center: [-98.47987660309634, 29.441973866341172],
         zoom: 15
     });
-
-
-
-
-
-
 
 
 });
