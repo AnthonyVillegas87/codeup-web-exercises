acae918968de5f3bd6b1c24502bc3a414e3874c6
Anthony Villegas <villegasanthony87@yahoo.com>
2021-08-20 16:22:04 -0500

    feat: js for weather map

--- js/weather.js
+++ js/weather.js
@@ -3,24 +3,44 @@ $(document).ready(() => {
 
     $.get('https://api.openweathermap.org/data/2.5/onecall', {
         appid: WEATHER_API_TOKEN,
-        lat: 29.4252,
-        lon: -98.4916,
+        lat: 29.44,
+        lon: -98.5,
         exclude: 'minutely,hourly,alerts',
         units: 'imperial',
         language: 'en'
     }).done((data) => {
-        console.log(data);
+        weather(data);
+
     }).fail((error) => {
         console.log(error);
     });
+
+    //let date = new Date(data.daily.dt * 1000).toDateString();
+
+
+
+    function weather(data) {
+        //let arr = Object.values(data.daily)
+        console.log(data)
+        $('#search').on('click', () => {
+            Object.data.forEach((data) => {
+                 $('li').append(`<div><ul><li>${data.dt}</li><li>${data.temp}</li><li>${data.feels_like}</li><li>${data.weather.main}</li><li>${data.weather.icon}</li></ul></div>`)
+            });
+        });
+
+    }
+
+
+
 
     mapboxgl.accessToken = MAPBOX_API_TOKEN;
     const map = new mapboxgl.Map({
         container: 'map',
         style: 'mapbox://styles/mapbox/streets-v11',
         center: [-98.47987660309634, 29.441973866341172],
-        zoom: 15
+        zoom: 6
     });
+
 
 
 });
