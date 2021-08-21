887b437f1baf25fd809b8a62978ffb7d91567411
Anthony Villegas <villegasanthony87@yahoo.com>
2021-08-21 15:30:05 -0500

    feat: rendered cards to HTML

--- js/weather.js
+++ js/weather.js
@@ -1,38 +1,6 @@
+
 $(document).ready(() => {
     "use strict";
-
-    $.get('https://api.openweathermap.org/data/2.5/onecall', {
-        appid: WEATHER_API_TOKEN,
-        lat: 29.44,
-        lon: -98.5,
-        exclude: 'minutely,hourly,alerts',
-        units: 'imperial',
-        language: 'en'
-    }).done((data) => {
-        weather(data);
-
-    }).fail((error) => {
-        console.log(error);
-    });
-
-    //let date = new Date(data.daily.dt * 1000).toDateString();
-
-
-
-    function weather(data) {
-        //let arr = Object.values(data.daily)
-        console.log(data)
-        $('#search').on('click', () => {
-            Object.data.forEach((data) => {
-                 $('li').append(`<div><ul><li>${data.dt}</li><li>${data.temp}</li><li>${data.feels_like}</li><li>${data.weather.main}</li><li>${data.weather.icon}</li></ul></div>`)
-            });
-        });
-
-    }
-
-
-
-
     mapboxgl.accessToken = MAPBOX_API_TOKEN;
     const map = new mapboxgl.Map({
         container: 'map',
@@ -40,6 +8,102 @@ $(document).ready(() => {
         center: [-98.47987660309634, 29.441973866341172],
         zoom: 6
     });
+    const marker = new mapboxgl.Marker({draggable: true})
+        .setLngLat([-98.47987660309634, 29.441973866341172])
+        //.setPopup(memorialPopup)
+        .addTo(map);
+    marker.on('dragend', () => {
+        document.querySelector('#coordinates').innerHTML = marker.getLngLat()
+    })
+
+    let day = document.getElementById('day');
+    let week = document.getElementById('week')
+
+
+    //start
+    let startCoordinates = {lat:29.4419,lon:-98.479}
+     weather(startCoordinates);
+
+    document.getElementById('button').addEventListener('click', function() {
+
+        weather(startCoordinates);
+    })
+
+// for data retrieval
+    function weather(obj) {
+        fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${obj.lat}&lon=${obj.lon}&exclude=minutely&units=imperial&appid=${WEATHER_API_TOKEN}`)
+            .then(result => result.json())
+            .then(data => {
+                console.log(data)
+                day.innerHTML = currentWeather(data.current)
+                week.innerHTML = weeklyWeather(data.daily)
+            })
+
+
+    }
+
+    // for current weather
+    function currentWeather(city) {
+        return `
+                <div class="card bg-info text-white mb-3 text-center">
+                    <div class="card-header"></div>
+                    <div class="card-body">
+                        <h4 class="card-title">${city.city}</h4>
+                        <ul>
+                            <li class="card-text">${city.dt}</li>
+                            <li class="card-text">${city.weather[0].description}</li>
+                            <li class="card-text">${city.temp}</li>
+                            <li class="card-text">${city.feels_like}</li>
+                            <li class="card-text">${city.humidity}</li>
+                            <li class="card-text">${city.weather[0].icon}</li>
+                        </ul>
+                    </div>
+               `
+    }
+//for weekly
+    function weeklyWeather(city) {
+        let html = '';
+        city.forEach((daily) => {
+            html +=  `<div class=" row-cols-5">
+            <div class="card-deck ">
+                <div class="card bg-info text-white mb-3 text-center">
+                    <div class="card-header"></div>
+                    <div class="card-body">
+                        <h4 class="card-title">${daily.city}</h4>
+                        <ul>
+                            <li class="card-text">${daily.dt}</li>
+                            <li class="card-text">${daily.weather[0].description}</li>
+                            <li class="card-text">${daily.temp}</li>
+                            <li class="card-text">${daily.feels_like}</li>
+                            <li class="card-text">${daily.humidity}</li>
+                            <li class="card-text">${daily.weather[0].icon}</li>
+                        </ul>
+                    </div>
+                </div>`
+
+        })
+         return  html
+    }
+
+    // function simplifyDate(data) {
+    //     let date = new Date(data.dt * 1000).toDateString();
+    //     console.log(data)
+    // }
+    // simplifyDate()
+
+
+
+//(`https://api.openweathermap.org/data/2.5/weather?APPID=${WEATHER_API_TOKEN}&q=${inputValue.value}`)
+
+
+
+
+
+
+
+
+
+
 
 
 
