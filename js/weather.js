1036a8c8439900a84a52cc3e8335286aa499ce15
Anthony Villegas <villegasanthony87@yahoo.com>
2021-08-22 22:24:35 -0500

    feat: cards made

--- js/weather.js
+++ js/weather.js
@@ -2,10 +2,12 @@
 $(document).ready(() => {
     "use strict";
 
-
+    //start
     let day = document.getElementById('day');
-    let week = document.getElementById('week')
+    let week = document.getElementById('week');
+    let startCoordinates = {lat:29.4419,lon:-98.479};
 
+    // Map Box
     mapboxgl.accessToken = MAPBOX_API_TOKEN;
     const map = new mapboxgl.Map({
         container: 'map',
@@ -14,25 +16,26 @@ $(document).ready(() => {
         zoom: 6
 
     });
+    document.querySelector('#button').addEventListener('click', () => {
+        let location = document.querySelector('#location').value;
+        geocode(location, MAPBOX_API_TOKEN).then((result) => {
+            marker.setLngLat(result);
+            map.flyTo({
+                center: result,
+            })
+            weather({lon: result[0], lat: result[1]})
+
+        });
+    });
     const marker = new mapboxgl.Marker({draggable: true})
         .setLngLat([-98.47987660309634, 29.441973866341172])
-        //.setPopup(memorialPopup)
         .addTo(map);
-    marker.on('dragend', () => {
-        document.querySelector('#coordinates').innerHTML = marker.getLngLat()
+    marker.on('dragend', (result) => {
+        // marker.getLngLat()
+        // weather({lon: result[0], lat: result[1]})
     })
 
 
-    //start
-    let startCoordinates = {lat:29.4419,lon:-98.479}
-     weather(startCoordinates);
-
-    document.getElementById('button').addEventListener('click', function() {
-
-
-
-        weather(startCoordinates);
-    })
 
 // for data retrieval
     function weather(obj) {
@@ -42,6 +45,7 @@ $(document).ready(() => {
                 console.log(data)
                 day.innerHTML = currentWeather(data.current)
                 week.innerHTML = weeklyWeather(data.daily)
+
             })
 
 
@@ -51,73 +55,49 @@ $(document).ready(() => {
     function currentWeather(city) {
         let currentDay = new Date(city.dt * 1000).toLocaleDateString('en', {weekday:'long'});
         let date = new Date(city.dt * 1000);
-        let iconCode = "https://openweathermap.org/img/w/" + city.weather[0].icon + ".png";
-
+        let icon =  city.weather[0].icon;
         return `
                 <div class="card bg-info text-white mb-3 text-center">
                     <div class="card-body">
                         <h4 class="card-title">${currentDay}</h4>
                             <li class="card-text">${date.toLocaleString()}</li>                   
                             <li class="card-text">${city.weather[0].description}</li>
-                            <li class="card-text">${city.temp}&#176</li>
-                            <li class="card-text">${city.feels_like}</li>
-                            <li class="card-text">${city.humidity}%</li>
-<!--                            <img src="">${iconCode}</li>                    -->
+                            <li class="card-text">Temp: ${city.temp}&#176</li>
+                            <li class="card-text">Feels like: ${city.feels_like}</li>
+                            <li class="card-text">Humidity: ${city.humidity}%</li>
+                            <div class="weather-icon"><img src="icons/icons/${icon}.png" /></div>
                     </div>
                `
     }
-    //for weekly
+    //for weekly weather
     function weeklyWeather(city) {
         let html = '';
-
         city.forEach((daily) => {
             let currentDay = new Date(daily.dt * 1000).toLocaleDateString('en', {weekday:'long'});
             let date = new Date(daily.dt * 1000);
-            html +=  `<div class=" row-cols-5">
-            <div class="card-deck ">
-                <div class="card bg-info text-white mb-3 text-center">
-                    <div class="card-body">
-                        <h4 class="card-title">${currentDay}</h4> 
+            let icon =  daily.weather[0].icon;
+            html +=
+                `                           
+                   <div class="row">
+        <div class="cols-12">
+                <div class="card-deck">
+                    <div class="card bg-info text-white mb-3 text-center">
+                        <div class="card-body">
+                            <h4 class="card-title">${currentDay}</h4>
                             <li class="card-text">${date.toLocaleString()}</li>
                             <li class="card-text">${daily.weather[0].description}</li>
-                            <li class="card-text">${daily.temp}&#176</li>
-                            <li class="card-text">${daily.feels_like}</li>
-                            <li class="card-text">${daily.humidity}%</li>
-                            <li class="card-text">${daily.weather[0].icon}</li>
+                            <li class="card-text">High: ${daily.temp.day}&#176</li>
+                            <li class="card-text">Low: ${daily.temp.eve}</li>
+                            <li class="card-text">Humidity: ${daily.humidity}%</li>
+                            <div class="weather-icon"> <img src="icons/icons/${icon}.png"></div>
+                        </div>
                     </div>
-                </div>`
-
+            </div>
+        </div>
+    </div> 
+                `
         })
          return  html
     }
-// Geocode function
-    function geocode(search, token) {
-        var baseUrl = 'https://api.mapbox.com';
-        var endPoint = '/geocoding/v5/mapbox.places/';
-        return fetch(baseUrl + endPoint + encodeURIComponent(search) + '.json' + "?" + 'access_token=' + token)
-            .then(function(res) {
-                return res.json();
-                // to get all the data from the request, comment out the following three lines...
-            }).then(function(data) {
-                return data.features[0].center;
-            });
-    }
-
-
-
-
-//(`https://api.openweathermap.org/data/2.5/weather?APPID=${WEATHER_API_TOKEN}&q=${inputValue.value}`)
-
-
-
-
-
-
-
-
-
-
-
-
 
 });
