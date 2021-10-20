d666158e8909f4b959a3ff86f3210318396b6475
Anthony Villegas <villegasanthony87@yahoo.com>
2021-10-20 07:51:36 -0500

    feat: creatElement() & appendChild() ex

--- js/dom-revisit.js
+++ js/dom-revisit.js
@@ -42,3 +42,8 @@ document.querySelector('h1').innerHTML = 'The .querySelector() method allows us 
 document.getElementById('four').innerHTML = '.getElementById() method to access the element with an id of "four".'
 
 document.querySelector('body').style.backgroundColor = '#201F2E';
+
+let newDestination = document.createElement('li');
+newDestination.id = 'six';
+newDestination.innerHTML = 'The .createElement(tagName) method creates a new element based on the specified tag name passed into it as an argument. However, it does not append it to the document. It creates an empty element with no inner HTML.In order to create an element and add it to the web page, you must assign it to be the child of an element that already exists on the DOM. We call this process appending. The .appendChild() method will add a child element as the last child node.'
+document.body.appendChild(newDestination);
