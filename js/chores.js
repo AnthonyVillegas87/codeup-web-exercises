e3e0e20959ae5c65580d2f048bf1fa6ffa6d5bce
Anthony Villegas <villegasanthony87@yahoo.com>
2021-10-20 22:44:58 -0500

    feat: js events for doo imgs

--- js/chores.js
+++ js/chores.js
@@ -1,1 +1,22 @@
 "use strict";
+
+let doorImage1 = document.getElementById("door1");
+let doorImage2 = document.getElementById("door2");
+let doorImage3 = document.getElementById("door3");
+
+
+let botDoorPath = "https://content.codecademy.com/projects/chore-door/images/robot.svg";
+let doorBeachPath = "https://content.codecademy.com/projects/chore-door/images/beach.svg";
+let spaceDoorPath = "https://content.codecademy.com/projects/chore-door/images/space.svg";
+
+doorImage1.onclick = () => {
+    doorImage1.src = botDoorPath;
+}
+
+doorImage2.onclick = () => {
+    doorImage2.src = doorBeachPath;
+}
+
+doorImage3.onclick = () => {
+    doorImage3.src = spaceDoorPath;
+}
